6ea115505ef9ccbc66947102d0c0d9af83cb92b6
agendreau <alex.gendreau24@gmail.com>
2015-02-13 09:32:38 -0700

    feature engineering homework

--- myRepo/features/category_guess.py
+++ myRepo/features/category_guess.py
@@ -1,5 +1,11 @@
+#Alex Gendreau
+#Machine Learning
+#Homework 3: Feature Engineering
+
+
 import argparse
 import string
+import re
 
 from csv import DictReader, DictWriter
 from random import shuffle
@@ -21,54 +27,44 @@ from nltk.util import ngrams
 
 import operator
 
+#analyzer that determines which features to use
 class Analyzer:
-    def __init__(self, word,length):
+    def __init__(self, word,cap,length):
         self.word = word
+        self.cap = cap
         self.length = length
     
-    def __call__(self, feature_string):
-        #print feature_string
-        #print
-        #feats = feature_string.split('$')
+    def __call__(self, feature_list):
+       
         
-        if self.word:
-            yield feature_string
-        
+        if self.cap:
+            cap = feature_list.pop()
+            yield cap
         if self.length:
-            yield feats[1]
-        '''
-        if self.after:
-            for ii in [x for x in feats if x.startswith("A:")]:
-                yield ii
-        if self.before:
-            for ii in [x for x in feats if x.startswith("B:")]:
-                yield ii
-        if self.prev:
-            for ii in [x for x in feats if x.startswith("P:")]:
-                yield ii
-        if self.next:
-            for ii in [x for x in feats if x.startswith("N:")]:
-                yield ii
-        if self.dict:
-            for ii in [x for x in feats if x.startswith("D:")]:
-                yield ii
-        if self.char:
-            for ii in [x for x in feats if x.startswith("C:")]:
-                yield ii
-        '''
+            l = feature_list.pop()
+            yield l
+        if self.word:
+            words = [x.strip() for x in feature_list]
+            for i in range(len(words)):
+                j = i+1
+                #k = i+2
+                if j<len(words)-1:
+                    yield words[i]+words[j]
+                #if k<len(words)-2:
+                    #yield words[i]+words[i+1]+words[i+2]
+                yield words[i]
+
 
 
 class Featurizer:
     def __init__(self):
-        analyzer = Analyzer(True,False)
-        #mostCommon = "boar theology discussion solomon ritual festival gods mythological built harbor feature mountain fault bridge red neutral parameter resistance physical classical physicist wavelength scattering quark physics background painter canvas architect foreground painting piano opera composer name russia convention battles byzantine execution successor capture foreign legislation empire radio clouds classification mass atmosphere object objects sun galaxies astronomical letter cursed decrease faith analyzes sermon study output kings thinker authors decides poems literature novel play stories literary playwright novels subset proof kind constructs methods abelian euler curve algebraic mathematician team season stanley code strip starring movies oscar lorentz laws largest above down constellation computer angle mechanics increase selection syndrome assay membrane cellular enzyme humans plants chromosome genes reaction reacts obtained molecular measured exponential molar presence chemistry chemist rock low boundary mantle period hot zone discontinuity mineral material"
-        #mostCommon = list(set(mostCommon.split(' ')))
-        stopwords = "the of and to a in for is on that by this with i you it not or be are from at as your all have new more an was we will home can us about if page my has search free but our one other do no information time they site he up may what which their news out use any there see only so his when contact here business who web also now help get pm view online first am been would how were me services some these click its like service than find"
-        stopwords = stopwords.split()
-        stopwords = text.ENGLISH_STOP_WORDS
-        self.vectorizer = CountVectorizer(ngram_range=(1,2),stop_words=stopwords)#,max_df=0.4)
-    
-    #self.vectorizer = VarianceThreshold(threshold=(.8 * (1 - .8)))
+        analyzer = Analyzer(True,True,True)
+        
+        self.vectorizer = CountVectorizer(analyzer=analyzer)
+        #Other stuff I messed around with
+        #,ngram_range=(2,2)) #,ngram_range=(1,3),stop_words=stopwords)
+        #,max_df=0.4)
+        #self.vectorizer = VarianceThreshold(threshold=(.8 * (1 - .8)))
 
     def train_feature(self, examples):
         test = self.vectorizer.fit_transform(examples)
@@ -85,35 +81,48 @@ class Featurizer:
             top10 = np.argsort(classifier.coef_[i])[-10:]
             print("%s: %s" % (category, " ".join(feature_names[top10])))
 
+#Modified from features.py.  All the possible features of an question
 def example(question,labels,train=True):
     #word = sentence[position][0]
     #ex = word
+    #Too many tokens potentially over fitting, how to reduce tokens but maintain results?
+    #Remove words and the padding from words
+    #Remove common words (i.e. by using stop words)
+    #Remove numbers
+    myStop = "the of and to a in for is on that by this with i you it not or be are from at as your all have new more an was we will home can us about if page my has search free but our one other do no information time they site he up may what which their news out use any there see only so his when contact here business who web also now help get pm view online first am been would how were me services some these click its like service than find"
+    myStop = set(myStop.split())
+    stopwords = text.ENGLISH_STOP_WORDS | myStop
     ex = question['text']
+    totalCap = 0
+    words = ex.split()
+    
+    toRemove = string.punctuation+','+'[\W_]'+';'
+    endings ='[ed][ing][\'s]'
+    words = [w.strip(toRemove) for w in words] #remove punctuation
+    words = [w.rstrip(endings) for w in words] #remove padding
+    words = [w.strip('\\n') for w in words] #for some reason weird newlines occurred
     
     
-    #sent = ex.split('.')
-    #length = len(sent)-1
-    #ex = ex.split(' ')
-    #ex = ex[0]
-    #ex = ex[len(ex)-2]
-    #ex = ex[0]
-    #tag = normalize_tags(sentence[position][1])
+    for w in words: #how many capital letters
+        if w.istitle():
+            totalCap+=1
+
+    wordsToUse = []
+    for w in words:
+        if w not in stopwords and len(w)>0:
+            wordsToUse.append(w)
+    length = len(ex.split('.'))-1 #number of sentences
+    wordsToUse.append(length)
+    wordsToUse.append(totalCap-length) #don't count first word of sentences
     if train:
         target = labels.index(question['cat'])
     else:
-        target = -1
-        #if tag in kTAGSET:
-        #target = kTAGSET.index(tag)
-        #else:
-        #target = None
-        
-        #ex+='$'
-#ex+=str(length)
+        target = -1 #unsurpervised, we don't know the info about our test set
     
-    return ex, target
+    return wordsToUse, target
 
 
-def all_examples(examples,limit,categories,train=True):
+def all_examples(examples,limit,categories,train=True): #from feature.py all examples
     ex_num = 0
     for ii in examples:
         ex_num += 1
@@ -125,7 +134,7 @@ def all_examples(examples,limit,categories,train=True):
         yield ex, tgt
 
 
-
+#measure the accuracy of the classifier using the confusion matrix
 def accuracy(classifier, x, y, examples,categories):
     predictions = classifier.predict(x)
     cm = confusion_matrix(y, predictions)
@@ -138,8 +147,6 @@ def accuracy(classifier, x, y, examples,categories):
         print("\t".join(str(x) for x in ii))
     
     errors = defaultdict(int)
-
-#print enumerate(examples)
 
     for ii, ex_tuple in enumerate(examples):
         #print ex_tuple
@@ -154,16 +161,25 @@ def accuracy(classifier, x, y, examples,categories):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='')
+    #argument determines how much of training set to use for actual training and
+    #how much to save for cross validation
     parser.add_argument('--limit', default=-1, type=int,
-                    help="How many sentences to use")
+                        help="How many sentences to use")
+    parser.add_argument('--compute', default=False, action='store_true',
+                        help="Actually Predict Test")
+    
     
     flags = parser.parse_args()
+    
 
     # Cast to list to keep it all in memory
     train = list(DictReader(open("train.csv", 'r')))
     totalTrain = len(train)
-    shuffle(train)
+    shuffle(train) #randomize test data so training and validation have similar distributions
     test = list(DictReader(open("test.csv", 'r')))
+    
+    if(flags.compute):
+        flags.limit = totalTrain
     
     feat = Featurizer()
 
@@ -171,8 +187,7 @@ if __name__ == "__main__":
     for line in train:
         if not line['cat'] in labels:
             labels.append(line['cat'])
-    #print labels
-    print
+
 
     x_train = feat.train_feature(ex for ex, tgt in
                                  all_examples(train[0:flags.limit],flags.limit,labels)) #all the words
@@ -184,14 +199,16 @@ if __name__ == "__main__":
     y_train = array(list(tgt for ex, tgt in
                          all_examples(train[0:flags.limit],flags.limit,labels)))
 
-    corr_x = feat.test_feature(ex for ex, tgt in
+#set up cross validation
+    if not flags.compute:
+        corr_x = feat.test_feature(ex for ex, tgt in
                                     all_examples(train[flags.limit:totalTrain],flags.limit,labels,train=True))
 
-    corr_y = array(list(tgt for ex, tgt in
+        corr_y = array(list(tgt for ex, tgt in
                         all_examples(train[flags.limit:totalTrain],flags.limit,labels)))
 
 
-    #print y_train
+
 
     # Train classifier
     lr = SGDClassifier(loss='log', penalty='l2', shuffle=True)
@@ -199,11 +216,12 @@ if __name__ == "__main__":
 
     feat.show_top10(lr, labels)
 
+#Training set and Cross Validation Set Accuracy
     print("TRAIN\n-------------------------")
     accuracy(lr, x_train, y_train,all_examples(train[0:flags.limit],flags.limit,labels),labels)
-
-    print("CORR\n-------------------------")
-    accuracy(lr, corr_x, corr_y,all_examples(train[flags.limit:totalTrain],flags.limit,labels),labels)
+    if not flags.compute:
+        print("CORR\n-------------------------")
+        accuracy(lr, corr_x, corr_y,all_examples(train[flags.limit:totalTrain],flags.limit,labels),labels)
 
     predictions = lr.predict(x_test)
     o = DictWriter(open("predictions.csv", 'w'), ["id", "cat"])
